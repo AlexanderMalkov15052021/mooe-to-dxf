c68b16fd9d683dce3deebb6ac8f2776174603ff9
Alex Malkov <malkov.fighter@yandex.ru>
2024-10-31 19:00:48 +0700

    fix layer position

--- src/modules/insert/setData.ts
+++ src/modules/insert/setData.ts
@@ -81,13 +81,15 @@ export const setData = (dxf: DxfWriter, mooe: MooeDoc) => {
     dxf.addRectangle(
         {
             x: (mooe?.mSceneMap?.mMapAttr?.mMapOrigin?.x ?? 1) / scaleCorrection
-                + (mooe?.mSceneMap?.mMapAttr?.mMapLength ?? 1) / scaleCorrection,
+                + (mooe?.mSceneMap?.mMapAttr?.mMapWidth ?? 1) / scaleCorrection,
+
             y: (mooe?.mSceneMap?.mMapAttr?.mMapOrigin?.y ?? 1) / scaleCorrection
         },
         {
             x: (mooe?.mSceneMap?.mMapAttr?.mMapOrigin?.x ?? 1) / scaleCorrection,
+            
             y: (mooe?.mSceneMap?.mMapAttr?.mMapOrigin?.y ?? 1) / scaleCorrection
-                + (mooe?.mSceneMap?.mMapAttr?.mMapWidth ?? 1) / scaleCorrection
+                + (mooe?.mSceneMap?.mMapAttr?.mMapLength ?? 1) / scaleCorrection
         },
         { layerName: "Layer" }
     );
